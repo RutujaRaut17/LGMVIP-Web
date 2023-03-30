b758bbd72c4d88c48e30ef72f0cf62a430717c64
RutujaRaut17 <114275127+RutujaRaut17@users.noreply.github.com>
2023-03-30 09:16:33 +0530

    second commit

---
+++ Calculator/script.js
@@ -0,0 +1,22 @@
+let result = document.getElementById('result');
+
+function getResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = '';
+}
+
+function backspace() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    alert('Invalid Calculation');
+    result.value = '';
+  }
+}
